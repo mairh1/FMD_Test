72f85d6b1e36cdfbe43be14e23d41a68552ad2ba
mairh1 <marmtrh@gmail.com>
2025-04-19 16:57:37 +0800

    test1

--- CX588_SOUND.C
+++ CX588_SOUND.C
@@ -4,14 +4,9 @@
 #include "CX588.h"
 
 #define VIBRATION_BUT PA2 //震动开关按键
-
-unsigned char ReadAPin;
-#define TIME_FLAG 5 //超时时间
-int TIME_OUT = 0;	//空闲计数值
-
 int Seed_Val = 0;	//假随机数种子
 
-void PA2_Level_Change_INITIAL();
+
 
 void Play_Sound(void)
 {
@@ -22,42 +17,13 @@ void Play_Sound(void)
 		{
 			if(CX588_Get_Busy_State())
 			{
-                srand(Seed_Val);		//生成随机数;
-                Seed_Val = Seed_Val + TIME_OUT;
-                CX588_Play_Sound((rand()%8)+1);
-                
+				CX588_Play_Sound((rand()%8)+1);
+                Seed_Val++;              
 			}
 		}		
 	}
 }
 
-void TIME_OUT_Enter_Sleep(void)
-{
-	if(CX588_Get_Busy_State() == 1)
-	{	
-		TMR2IE = 1;						//使能TIMER2的中断  
-		PEIE=1;    						//使能外设中断
-		GIE = 1;   						//使能全局中断
-		if(TIME_OUT > TIME_FLAG)
-		{
-			TIME_OUT = 0;//清空计数值
-			TMR2IE = 0;						//失能TIMER2的中断  
-			PEIE = 0;    					//失能外设中断
-			GIE = 0;   						//失能全局中断
-			
-            PA2_Level_Change_INITIAL();
-			SLEEP(); 						//进入休眠模式
-			NOP();							//SLEEP()后面要接NOP()
-		}
-	}
-	else
-	{
-		TIME_OUT = 0;//清空计数值
-		TMR2IE = 0;						//失能TIMER2的中断  
-		PEIE = 0;    					//失能外设中断
-		GIE = 0;   						//失能全局中断
-	}
-}
 /*-------------------------------------------------
  * 函数名：interrupt ISR
  * 功能：  定时器2的中断处理
@@ -66,23 +32,7 @@ void TIME_OUT_Enter_Sleep(void)
  --------------------------------------------------*/	
 void interrupt ISR(void)
 {
-	if(TMR2IE && TMR2IF)			//1s中断一次 = 1Hz
-	{
-		TMR2IF = 0;
-        
-		if(TIME_OUT < 0xff)
-			TIME_OUT++;   
-	} 
-//         
-//    //PA电平变化中
-//	if(PAIE && PAIF)		
-//    {
-//		ReadAPin = PORTA; 		//读取PORTA数据清PAIF标志
-//		PAIF = 0;  				//清PAIF标志位
-//		PAIE = 0;  				//暂先禁止PA0中断
-//		IOCA2 =0;  				//禁止PA0电平变化中断
-//        GIE = 1;   						//使能全局中断
-//    }
+
 }
 
 /*-------------------------------------------------
@@ -97,47 +47,11 @@ void POWER_INITIAL (void)
 	INTCON = 0;  			//暂禁止所有中断
     
 	OPTION = 0;
-    TRISA = 0B00000100;				//1:输入 0:输出 
+    TRISA = 0B00000100;		//1:输入 0:输出 
     PSRCA = 0;				//00:4mA	01/10:8mA	11:28mA	bit[3:2]控制PA5源电流	bit[1:0]控制PA4源电流
     PSINKA = 0;				//bit[1:0]	控制PA5和PA4 0:灌电流最小 1:灌电流最大
-    PORTA = 0B00000000;				//1:PAx输出高电平	0:PAx输出低电平	
- 	WPUA  = 0B00000100;				//1:使能PA口上拉	0:关闭PA口上拉  
-}
-/*-------------------------------------------------
- * 函数名：TIMER2_INITIAL
- * 功能：  初始化设置定时器1 
- * 设置Timer2定时时长 = (1/系统时钟频率)*4*预分频值*后分频值*PR2
- *					  = (1/16000000)*4*16*10*25000=1000000us=1S
- -------------------------------------------------*/
-void TIMER2_INITIAL (void) 
-{
-	T2CON0 = 0B01001011;
-	//Bit[6:3]=0000,T2时钟后分频比1:10
-    //Bit[1:0]=01,T2时钟预分频比1:16
-    
-    T2CON1 = 0B00001000;			//Bit[2:0] 000:	指令周期 100:HIRC Timer2时钟源选择	
-    TMR2H = 0;
-	TMR2L = 0;  					//TMR2赋初值
-    PR2H = 100;
-	PR2L = 250; 					//设置PR2=25000
-    
-	TMR2IF = 0;						//清TIMER2中断标志
-	TMR2ON = 1;						//使能TIMER2启动
-
-}
-/*-------------------------------------------------
- * 函数名: PA2_Level_Change_INITIAL
- * 功能：  PA端口(PA2)电平变化中断初始化
- * 输入：  无
- * 输出：  无
---------------------------------------------------*/
-void PA2_Level_Change_INITIAL(void)
-{
-	TRISA2 = 1; 			     	//设置 PA2 输入
-	ReadAPin = PORTA;	     		//清PA电平变化中断
-	PAIF = 0;   			     	//清PA INT中断标志位
-    IOCA2 = 1;  			     	//使能PA2电平变化中断
-	PAIE = 1;   			     	//使能PA INT中断
+    PORTA = 0B00000000;		//1:PAx输出高电平	0:PAx输出低电平	
+ 	WPUA  = 0B00000100;		//1:使能PA口上拉	0:关闭PA口上拉  
 }
 /*-------------------------------------------------
  * 函数名：main 
@@ -148,15 +62,14 @@ void PA2_Level_Change_INITIAL(void)
 void main()
 {
  	POWER_INITIAL();		//系统初始化
-	TIMER2_INITIAL();		//初始化定时器2
+
 	CX588_GPIO_Init();		//初始化CX588
     CX588_SET_Sound_Size(SoundLeve_10);
     srand(Seed_Val);		//生成随机数;
     
 	while(1)
 	{
-		Play_Sound();
-		//TIME_OUT_Enter_Sleep();
-        
+		Play_Sound();  
+        srand(Seed_Val);		//生成随机数;    
 	}   	
 }
