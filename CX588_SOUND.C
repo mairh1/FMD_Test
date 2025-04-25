8d7bf46df86291d24de348e033655615fb94279c
mairh1 <marmtrh@gmail.com>
2025-04-25 16:24:14 +0800

    增加EEPROM记忆

--- CX588_SOUND.C
+++ CX588_SOUND.C
@@ -27,6 +27,7 @@ void TIME_OUT_Enter_Sleep(void)
 			TMR2IE = 0;						//失能TIMER2的中断  
 			PEIE = 0;    					//失能外设中断
 			GIE = 0;   						//失能全局中断
+            EEPROMwrite(Seed_EEPROM_ADD,Seed_Val);//写入随机种子地址
 			
             PA2_Level_Change_INITIAL();
 			SLEEP(); 						//进入休眠模式
@@ -93,6 +94,49 @@ void interrupt ISR(void)
 }
 
 /*-------------------------------------------------
+ * 函数名：EEPROMread
+ * 功能：  读EEPROM数据
+ * 输入：  需要读取数据的地址 EEAddr
+ * 输出：  对应地址读出的数据 ReEEPROMread
+ --------------------------------------------------*/
+unsigned char EEPROMread(unsigned char EEAddr)
+{
+	unsigned char ReEEPROMread;
+    EEADR = EEAddr;
+    RD=1;
+    NOP();
+    NOP();
+    NOP();
+    NOP();
+    ReEEPROMread =EEDAT;
+    return ReEEPROMread;
+}
+
+/*-------------------------------------------------
+ * 函数名：EEPROMwrite
+ * 功能：  写数据到EEPROM
+ * 输入：  需要读取数据的地址 EEAddr
+		   需要写入的数据	Data
+ * 输出：  无
+ --------------------------------------------------*/
+void EEPROMwrite(unsigned char EEAddr,unsigned char Data)
+{
+	GIE = 0;					//写数据必须关闭中断
+	while(GIE); 				//等待GIE为0
+	EEADR = EEAddr; 	 		//EEPROM的地址
+	EEDAT = Data;		 		//EEPROM的写数据  EEDATA = Data;
+	EEIF = 0;
+	EECON1 |= 0x34;				//置位WREN1,WREN2,WREN3三个变量.
+	WR = 1;						//置位WR启动
+    NOP();
+    NOP();
+    NOP();
+    NOP();
+	while(WR);      			//等待EE写入完成
+	GIE = 1;
+}
+
+/*-------------------------------------------------
  * 函数名: PA2_Level_Change_INITIAL
  * 功能：  PA端口(PA2)电平变化中断初始化
  * 输入：  无
@@ -145,7 +189,7 @@ void POWER_INITIAL (void)
     PSRCA = 0;				//00:4mA	01/10:8mA	11:28mA	bit[3:2]控制PA5源电流	bit[1:0]控制PA4源电流
     PSINKA = 0;				//bit[1:0]	控制PA5和PA4 0:灌电流最小 1:灌电流最大
     PORTA = 0B00000000;		//1:PAx输出高电平	0:PAx输出低电平	
- 	WPUA  = 0B00000100;		//1:使能PA口上拉	0:关闭PA口上拉  
+ 	WPUA  = 0B00000000;		//1:使能PA口上拉	0:关闭PA口上拉  
 }
 /*-------------------------------------------------
  * 函数名：main 
@@ -158,7 +202,8 @@ void main()
  	POWER_INITIAL();		//系统初始化
 	TIMER2_INITIAL();		//初始化定时器
 	CX588_GPIO_Init();		//初始化CX588
-    CX588_SET_Sound_Size(SoundLeve_10);
+    CX588_SET_Sound_Size(SoundLeve_15);//设置CX588音量最大
+    Seed_Val = EEPROMread(Seed_EEPROM_ADD);//从eeprom中读取随机数种子
     srand(Seed_Val);		//生成随机数;
     
 	while(1)
--- CX588_SOUND.h
+++ CX588_SOUND.h
@@ -2,7 +2,12 @@
 #define CX588_SOUND_H
 
 #define VIBRATION_BUT PA2 //震动开关按键
-#define TIME_FLAG 5 //定时器超时时间 5*1S=5S
+#define TIME_FLAG 3 //定时器超时时间 3*1S=3S
+
+#define Seed_EEPROM_ADD 0x00 //随机种子地址
+
+unsigned char EEPROMread(unsigned char EEAddr);
+void EEPROMwrite(unsigned char EEAddr,unsigned char Data);
 
 void PA2_Level_Change_INITIAL();
 void TIME_OUT_Enter_Sleep(void);
